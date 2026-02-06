7cf8712370daf86d1f70534b35a983d6b11f1e84
Naveen13189 <naveen.polina98@gmail.com>
2026-02-06 08:59:12 +0530

    Reactjs practice

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+//primittive data types
+
+var a = 10;
+console.log(typeof a);
+var b = "Naveen";
+console.log(typeof b);
+
+var boyName = "Naveen Siri"
+
+//null
+var c = null;
+console.log(typeof c);
+
+var d = true;
+console.log(typeof d);
+
+var e;
+console.log(typeof e);
+
+//non primitive datatype
+
+var arr = [1, 3, 5, 7, 9]
+
+//Each element number starts with 0 1 2 3 4 .....
+console.log(arr[3])
+
+var obj = {
+    name: "naveen",
+    //name: "naveen polina",
+    role: "dev",
+    class: "developer",
+};
+console.log(obj.name);
+console.log(obj["role"]);
