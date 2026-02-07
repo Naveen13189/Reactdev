660b8418990f1a4bb84601d11e5e70798cfcbc4b
Naveen13189 <naveen.polina98@gmail.com>
2026-02-07 17:26:44 +0530

    practice java

---
+++ practiceDatatypes.js
@@ -0,0 +1,24 @@
+var a = 10;
+console.log(typeof a);
+var b = "Naveen"
+console.log(typeof b);
+
+//null
+var c = null;
+console.log(typeof c);
+
+var d = true;
+console.log(typeof d);
+
+var e;
+console.log(typeof e);
+
+var arry = [3, 5,67, 7,9];
+console.log(arry[2]);
+
+var obj = {
+    name: "Naveen",
+    role: "dev",
+    class: "Java",
+};
+console.log(obj.name);
---
+++ practiceupdatingObjects.js
@@ -0,0 +1,9 @@
+const arr = [1, 74, 84,93, 86]
+arr.push(100, 583);
+//console.log(arr);
+
+function f1(data) {
+    console.log("function executed");
+    console.log(data);
+}
+f1(arr);
